97fe60a52de77ce0c813cdfb2edf0888ee6b194e
James Benson <james.l.benson8@gmail.com>
2023-06-05 20:40:06 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by James Benson on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
